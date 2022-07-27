0d02e5ff3171878f2430934092c45e70981b459e
ShantanuDaz <shaunofshan@gmail.com>
2022-07-27 10:17:31 +0530

    intial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./layout.css";
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="info glass">
+          <div className="profilePic"></div>
+          <h1>"Hello World" My Name Is Shantanu Das</h1>
+          <p>Trying To Build Future But Not Just For Me</p>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
