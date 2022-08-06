f2a15d13a1579ede8c5b88f1f8299f47b3d4e3a6
ShantanuDaz <shaunofshan@gmail.com>
2022-08-07 00:53:32 +0530

    theme changed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,13 +3,19 @@ import "./layout.css";
 const Header = () => {
   return (
     <>
-      <header>
+      <section>
         <div className="info glass">
           <div className="profilePic"></div>
           <h1>"Hello World" My Name Is Shantanu Das</h1>
           <p>Trying To Build Future But Not Just For Me</p>
+          <nav>
+            <button className="glass">Projects</button>
+            <button className="glass">Skills</button>
+            <button className="glass">About</button>
+            <button className="glass">Contact</button>
+          </nav>
         </div>
-      </header>
+      </section>
     </>
   );
 };
